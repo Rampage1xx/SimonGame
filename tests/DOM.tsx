1ee110c23503b75e0be894d9234253eae4f06876
rampage1xx <godhasu@gmail.com>
2017-05-24 10:26:46 +0200

    Adding Jest/Enzyme tests

--- tests/DOM.tsx
+++ tests/DOM.tsx
@@ -1,12 +1,18 @@
 import {mount, shallow} from 'enzyme';
 import * as React from 'react';
 import {SimonColor} from '../ClientSRC/Components/SimonColor';
-import {InnerCircle} from '../ClientSRC/Components/InnerCircle';
 
 describe('testing the SimonColor Component', () => {
-    const SimonComponent = <SimonColor colorNumber={1} css={'test'} ON={true} children playerTurn={true}/>;
+    const propsObject = {
+        colorNumber: 1,
+        css: 'test',
+        ON: true,
+        playerTurn: true
+    };
+    const SimonComponent = <SimonColor {...propsObject}/>;
     const simonColorShallow = shallow(SimonComponent);
-    const simonColorMount = mount(SimonComponent)
+    const simonColorMount = mount(SimonComponent);
+
     it('should  shallow render', () => {
         expect(simonColorShallow.shallow()).toBeTruthy();
     });
@@ -14,11 +20,14 @@ describe('testing the SimonColor Component', () => {
         expect(simonColorShallow.props().className).toEqual('test');
     });
     it('should mount', () => {
-        console.log(simonColorMount.debug())
-        expect(simonColorMount).toBeTruthy()
-    })
+        expect(simonColorMount).toBeTruthy();
+    });
+    it('should have the props', () => {
+        expect(simonColorMount.props()).toEqual(propsObject);
+    });
+
 });
 
 describe('testing the InnerCircle Component', () => {
-   // const innerCircleShallow = shallow(<InnerCircle playerTurn={true}/>)
+    // const innerCircleShallow = shallow(<InnerCircle playerTurn={true}/>)
 });
