2e65fb029bd56b53cabaa4e57be1b8266474a1e9
rampage1xx <godhasu@gmail.com>
2017-05-24 12:45:13 +0200

    Adding Jest/Enzyme tests

--- tests/Actions.tsx
+++ tests/Actions.tsx
@@ -1,60 +1,114 @@
 import {
     actionActiveColor, actionClearLists, actionComputerHasGeneratedColor, actionGameOver, actionGameStarted,
-    actionGameStopped,
-    actionPlayerHasChosenColor,
-    actionPlayerTurn, actionReplay, actionResetLimit, actionTurnOff, actionTurnOn, actionStrictMode
+    actionGameStopped, actionPlayerHasChosenColor, actionPlayerTurn, actionPlayerVictory, actionReplay,
+    actionResetLimit, actionStrictMode, actionTurnOff, actionTurnOn
 } from '../ClientSRC/Actions/ActionCreators';
 import {
     ACTIVE_COLOR, CLEAR_BOTH_LISTS, COLOR_REPLAY, COMPUTER_GENERATED_COLOR, GAME_OVER, GAME_STARTED,
-    PLAYER_GENERATED_COLOR,
-    PLAYER_TURN, RESET_LIMIT, TURN_OFF, TURN_ON, STRICT_MODE
+    PLAYER_GENERATED_COLOR, PLAYER_TURN, PLAYER_VICTORY, RESET_LIMIT, STRICT_MODE, TURN_OFF, TURN_ON
 } from '../ClientSRC/Actions/ActionTypes';
+
+interface IITStatementFactory {
+
+    (parameter: {
+        message: string,
+        valueToCheck: object,
+        expectedValue: object
+    })
+
+}
+
 describe('Testing actions  creation', () => {
 
-    const messageFactory = (message) => `should create an action for the ${message}`;
+    const itStatementFactory: IITStatementFactory = ({message, valueToCheck, expectedValue}) =>
+        it(`should create an action for the ${message}`, () => {
+            expect(valueToCheck).toEqual(expectedValue);
+        });
 
-    const parameters = (type: string, actionParameters: object) => ({type, ...actionParameters});
-
-    const expectToEqual = (value: object, expected: object) => expect(value).toEqual(expected);
-
-    it(messageFactory('player chosen color'), () => {
-        expectToEqual(actionPlayerHasChosenColor(1), {type: PLAYER_GENERATED_COLOR, color: 1});
-    });
-    it(messageFactory('computer chosen color'), () => {
-        expectToEqual(actionComputerHasGeneratedColor(3), {type: COMPUTER_GENERATED_COLOR, color: 3});
-    });
-    it(messageFactory('Game Over'), () => {
-        expectToEqual(actionGameOver(), {type: GAME_OVER});
-    });
-    it(messageFactory('Clear List'), () => {
-        expectToEqual(actionClearLists(), {type: CLEAR_BOTH_LISTS});
-    });
-    it(messageFactory('Active Color'), () => {
-        expectToEqual(actionActiveColor(2), {type: ACTIVE_COLOR, color: 2});
-    });
-    it(messageFactory('player turn'), () => {
-        expectToEqual(actionPlayerTurn(true), {type: PLAYER_TURN, playerTurn: true});
-    });
-    it(messageFactory('reset limit'), () => {
-        expectToEqual(actionResetLimit(), {type: RESET_LIMIT});
-    });
-    it(messageFactory('turn off'), () => {
-        expectToEqual(actionTurnOff(), {type: TURN_OFF});
-    });
-    it(messageFactory('turn on'), () => {
-        expectToEqual(actionTurnOn(), {type: TURN_ON});
-    });
-    it(messageFactory(' replay'), () => {
-        expectToEqual(actionReplay(1, 2), {type: COLOR_REPLAY, color: 1, index: 2});
-    });
-    it(messageFactory('started'), () => {
-        expectToEqual(actionGameStarted(), {type: GAME_STARTED, value: true});
-    });
-    it(messageFactory('stopped'), () => {
-        expectToEqual(actionGameStopped(), {type: GAME_STARTED, value: false});
-    });
-    it(messageFactory('strict mode'), () => {
-        expectToEqual(actionStrictMode(true), {type: STRICT_MODE, value: true});
+    itStatementFactory({
+        message: 'player chosen color',
+        valueToCheck: actionPlayerHasChosenColor(1),
+        expectedValue: {type: PLAYER_GENERATED_COLOR, color: 1}
     });
 
+    itStatementFactory({
+        message: 'computer chosen color',
+        valueToCheck: actionComputerHasGeneratedColor(3),
+        expectedValue: {type: COMPUTER_GENERATED_COLOR, color: 3}
+    });
+
+    itStatementFactory({
+        message: 'Game Over',
+        valueToCheck: actionGameOver(),
+        expectedValue: {type: GAME_OVER}
+    });
+
+    itStatementFactory({
+        message: 'Clear List',
+        valueToCheck: actionClearLists(),
+        expectedValue: {type: CLEAR_BOTH_LISTS}
+    });
+
+    itStatementFactory({
+        message: 'Active Color',
+        valueToCheck: actionActiveColor(2),
+        expectedValue: {type: ACTIVE_COLOR, color: 2}
+    });
+
+    itStatementFactory({
+        message: 'player turn',
+        valueToCheck: actionPlayerTurn(true),
+        expectedValue: {type: PLAYER_TURN, playerTurn: true}
+    });
+
+    itStatementFactory({
+        message: 'reset limit',
+        valueToCheck: actionResetLimit(),
+        expectedValue: {type: RESET_LIMIT}
+    });
+
+    itStatementFactory({
+        message: 'turn off',
+        valueToCheck: actionTurnOff(),
+        expectedValue: {type: TURN_OFF, on: false}
+    });
+
+    itStatementFactory({
+        message: 'turn on',
+        expectedValue: actionTurnOn(),
+        valueToCheck: {type: TURN_ON, on: true}
+    });
+
+    itStatementFactory({
+        message: 'replay',
+        valueToCheck: actionReplay(1, 2),
+        expectedValue: {type: COLOR_REPLAY, color: 1, index: 2}
+    });
+    itStatementFactory({
+        message: 'started',
+        valueToCheck: actionGameStarted(),
+        expectedValue: {type: GAME_STARTED, value: true}
+    });
+
+    itStatementFactory({
+        message: 'stopped',
+        valueToCheck: actionGameStopped(),
+        expectedValue: {type: GAME_STARTED, value: false}
+    });
+    itStatementFactory({
+        message: 'strict mode',
+        valueToCheck: actionStrictMode(true),
+        expectedValue: {type: STRICT_MODE, value: true}
+    });
+    itStatementFactory({
+        message: 'player turn',
+        valueToCheck: actionPlayerVictory(),
+        expectedValue: {type: PLAYER_VICTORY}
+    });
+
+    itStatementFactory({
+        message: 'clear list',
+        valueToCheck: actionClearLists(),
+        expectedValue: {type: CLEAR_BOTH_LISTS}
+    });
 });
